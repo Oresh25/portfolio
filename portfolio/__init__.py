5632d4f854dbaabe81af8bb74018c919f3cd33e4
Or <oreshed2580@gmail.com>
2023-07-18 18:50:44 +0300

    Create_app

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,60 +1,64 @@
 from flask import Flask, render_template,abort
 
-app = Flask(__name__)
 
-projects = [
-    {
-        "name": "REST API",
-        "thumb": "img/REST-hero.JPG",
-        "hero": "img/REST.JPG",
-        "categories": ["python", "Flask", "Docker"],
-        "slug": "REST-API",
-        "prod": "https://github.com/Oresh25/REST_API"
-    },
-    {
-        "name": "MicroBlog app",
-        "thumb": "img/microblog.jpg",
-        "hero": "img/microblog.jpeg",
-        "categories": ["python", "Flask", "html", "css", "MongoDB"],
-        "slug": "MicroBlog",
-        "prod": "https://github.com/Oresh25/Web-MicroBlog"
-    },
-    {
-        "name": "Cocktail web",
-        "thumb": "img/cocktail.JPG",
-        "hero": "img/cocktail_about.jpg",
-        "categories": ["html", "css", "JAVASCRIPT", "PHP"],
-        "slug": "Cocktail_web",
-        "prod": "https://github.com/Oresh25/Web-Project"
-    }
-]
+def create_app():
+    app = Flask(__name__)
 
-slug_to_project = {project["slug"]: project for project in projects}
+    projects = [
+        {
+            "name": "REST API",
+            "thumb": "img/REST-hero.JPG",
+            "hero": "img/REST.JPG",
+            "categories": ["python", "Flask", "Docker"],
+            "slug": "REST-API",
+            "prod": "https://github.com/Oresh25/REST_API"
+        },
+        {
+            "name": "MicroBlog app",
+            "thumb": "img/microblog.jpg",
+            "hero": "img/microblog.jpeg",
+            "categories": ["python", "Flask", "html", "css", "MongoDB"],
+            "slug": "MicroBlog",
+            "prod": "https://github.com/Oresh25/Web-MicroBlog"
+        },
+        {
+            "name": "Cocktail web",
+            "thumb": "img/cocktail.JPG",
+            "hero": "img/cocktail_about.jpg",
+            "categories": ["html", "css", "JAVASCRIPT", "PHP"],
+            "slug": "Cocktail_web",
+            "prod": "https://github.com/Oresh25/Web-Project"
+        }
+    ]
 
-@app.route("/")
-def home():
-    return render_template("home.html", projects=projects)
+    slug_to_project = {project["slug"]: project for project in projects}
 
-
-@app.route("/about")
-def about():
-    return render_template("about.html")
+    @app.route("/")
+    def home():
+        return render_template("home.html", projects=projects)
 
 
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
-@app.route("/projects/<string:slug>")
-def project(slug):
-    if slug not in slug_to_project:
-        abort(404)
-    return render_template(
-        f"project_{slug}.html", 
-        project=slug_to_project[slug]
-    )
+    @app.route("/about")
+    def about():
+        return render_template("about.html")
 
 
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template("404.html"), 404
+    @app.route("/contact")
+    def contact():
+        return render_template("contact.html")
+
+    @app.route("/projects/<string:slug>")
+    def project(slug):
+        if slug not in slug_to_project:
+            abort(404)
+        return render_template(
+            f"project_{slug}.html", 
+            project=slug_to_project[slug]
+        )
+
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template("404.html"), 404
+    
+    return app
